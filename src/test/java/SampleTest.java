a69d4efce282a2a5b8dfd3d1b57deb991b6b6a1e
tertychnyy <hogords@gmail.com>
2020-07-24 14:05:19 +0300

    third

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -2,6 +2,7 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.Step;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.demoPage;
 
@@ -11,6 +12,7 @@ import static com.codeborne.selenide.Selenide.page;
 public class SampleTest extends BaseTest {
 
     @Test
+    @Tag("UI")
     @Severity(SeverityLevel.MINOR)
     @Description("¬ыход из демо режима в полный режим")
     @Step("¬ыдаем разрешени€ дл€ приложени€ / переходим в меню / переводим приложение в 'полный режим'")
